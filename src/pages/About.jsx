6f5f24bbb1c92fc62575ea523e2134fd8f32aa0b
Jennie Nguyen <jentdev@gmail.com>
2025-03-29 18:03:00 -0500

    removed react responsive modal

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import AboutModal from '../modals/AboutModal';
 
 export const About = () => {
   return (
     <div>
       <div>about me</div>
-      <AboutModal />
+
     </div>
   )
 };
