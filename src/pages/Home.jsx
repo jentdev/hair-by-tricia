9bfcc6e2dc2baa83ab7e20cdd5816e4f708bf5a4
Jennie Nguyen <jentdev@gmail.com>
2025-03-30 23:11:02 -0500

    minor ui touch-up

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,15 +3,12 @@ import React from 'react';
 export const Home = () => {
   return (
     <main>
-      <div className="welcome-text">      
-      <div>
-      <p>Welcome to Hair by Tricia, your go-to destination for expert hair care and styling.</p>
-      <p>Whether you're looking for a fresh cut, vibrant color, or a bold new look, our skilled team is here to bring your hair vision to life.</p>
-      <p>With a focus on personalized service and a relaxing atmosphere, we’re dedicated to making every visit a rejuvenating experience.</p>
-      <p>Explore our services and book your appointment today — your perfect hair awaits!</p>
-
+      <div className="welcome-text">
+        {/* <p>Welcome to Hair by Tricia, where beauty meets style.</p>
+        <p>Our expert team is dedicated to creating personalized, stunning looks that make you feel your best.</p>
+        <p>Let us transform your hair and bring your vision to life!</p> */}
       </div>
-      </div>
+      <div className="white-rect"></div>
     </main>
   )
 };
