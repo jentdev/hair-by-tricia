c9bb98b6eb235190ed097274d5765d02f836034d
Jennie Nguyen <jentdev@gmail.com>
2025-03-31 17:08:06 -0500

    fixed navbar position

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,23 +8,25 @@ import { assets, navItems } from "../assets/assets";
 export const Navbar = () => {
   return (
     <nav>      
-        <div className="logo">
-          logo
-          {/* <img src={assets.bg} className="w-15 h-15" alt="" /> */}
-        </div>
+        <div className="logo-nav">
+          <div className="logo">
+            logo
+            {/* <img src={assets.bg} className="w-15 h-15" alt="" /> */}
+          </div>
 
-        <div className="navlinks">
-          {/* nav items */}
-          <ul>
-            {navItems.map(({ label, endpoint }) => (
-              <li key={label}>
-                <NavLink to={endpoint}>{label}</NavLink>
-              </li>
-            ))}
-          </ul>
+          <div className="navlinks">
+            {/* nav items */}
+            <ul>
+              {navItems.map(({ label, endpoint }) => (
+                <li key={label}>
+                  <NavLink to={endpoint}>{label}</NavLink>
+                </li>
+              ))}
+            </ul>
 
-          {/* hamburger menu */}
-          <MobileMenu />
+            {/* hamburger menu */}
+            <MobileMenu />
+          </div>
         </div>
 
     </nav>
