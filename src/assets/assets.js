93ed65845e59d67be69097cfd16df52d8bdfc982
Jennie Nguyen <jentdev@gmail.com>
2025-04-01 16:52:03 -0500

    updated location page

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,9 +1,11 @@
 import bg from './bg.jpg';
 import logo from './logo.svg';
+import pin from './pin.svg';
 
 export const assets = {
     bg,
     logo,
+    pin,
 };
 
 export const navItems = [
--- src/pages/Location.jsx
+++ src/pages/Location.jsx
@@ -1,17 +1,33 @@
-import React from 'react'
+import React from 'react';
+import 'leaflet/dist/leaflet.css';
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import { Icon } from 'leaflet';
+
+import { assets } from '../assets/assets';
 
 export const Location = () => {
+
+  const marker = {
+    geacode: [28.0868093,-82.7816883],
+    popUp: 'Hair by Tricia'
+  };
+
+  const customIcon = new Icon({
+    iconUrl: assets.pin,
+    iconSize: [38, 38] // size of icon
+  });
+
   return (
-    <div>
+    <div className="location">
 
       <div className="our-location">
-        <h2>our location</h2>
+        <h1>our location</h1>
         <p><span>Hair by Tricia</span> is conveniently located and ready to help you look and feel your best.</p>
       </div>
 
       <div className="address">
         <h1>address</h1>
-        <p>Hair by Tricia</p>
+        <h3>Hair by Tricia</h3>
         <p>698 Dodecanese Blvd</p>
         <p>Tarpon Springs, FL 34689</p>
       </div>
@@ -19,15 +35,26 @@ export const Location = () => {
       <div className="hours">
         <h1>business hours</h1>
         <ul>
-          <li><span>monday - friday:</span> <span>9:00am - 6:00pm</span></li>
-          <li><span>Saturday:</span> <span>9:00am - 7:00pm</span></li>
-          <li><span>sunday:</span> <span>closed</span></li>
+          <li><span className="dates">monday - saturday:</span> <span>9:00am - 6:00pm</span></li>        
+          <li><span className="dates">sunday:</span> <span>closed</span></li>
         </ul>
       </div>
 
       <div className="map">
-        <p>find us on the map below:</p>
-        <div className="leaflet-map"></div>
+        <p>Find us on the map below:</p>
+        <div className="leaflet-map">
+
+        <MapContainer center={[28.0868093,-82.7816883]} zoom={13}>
+          <TileLayer
+            url='https://tile.openstreetmap.org/{z}/{x}/{y}.png'
+          />
+
+          <Marker position={marker.geacode} icon={customIcon}>
+            <Popup>{marker.popUp}</Popup>
+          </Marker>         
+        </MapContainer>
+
+        </div>
       </div>
 
     </div>
