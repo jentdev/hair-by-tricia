891450412a695260cd738b1b610c0b37b0e20485
Jennie Nguyen <jentdev@gmail.com>
2025-03-31 21:00:28 -0500

    added price list

---
+++ src/assets/priceListAssets.js
@@ -0,0 +1,124 @@
+export const haircuts = [
+    {
+        style: "women's haircut",
+        min: 45,
+        max: 75,
+    },
+    {
+        style: "men's haircut",
+        min: 25,
+        max: 45,
+    },
+    {
+        style: "children's haircut (under 12)",
+        min: 45,
+        max: 75,
+    },
+    {
+        style: "senior haircut",
+        min: 25,
+        max: 40,
+    },
+    {
+        style: "blowout",
+        min: 30,
+        max: 50,
+    },
+    {
+        style: "shampoo & style",
+        min: 25,
+        max: 40,
+    },
+    {
+        style: "updo/formal styling",
+        min: 50,
+        max: 85,
+    },
+];
+
+export const hairColor = [
+    {
+        style: "single process color",
+        min: 60,
+        max: 90,
+    },
+    {
+        style: "root touch-up",
+        min: 45,
+        max: 75,
+    },
+    {
+        style: "partial highlights",
+        min: 70,
+        max: 120,
+    },
+    {
+        style: "full highlights",
+        min: 100,
+        max: 150,
+    },
+    {
+        style: "balayage",
+        min: 120,
+        max: 180,
+    },
+];
+
+export const hairTreatments = [
+    {
+        style: "deep conditioning",
+        min: 25,
+        max: 40,
+    },
+    {
+        style: "keratin smoothing",
+        min: 150,
+        max: 300,
+    },
+    {
+        style: "scalp",
+        min: 30,
+        max: 50,
+    },
+    {
+        style: "alaplex",
+        min: 40,
+        max: 70,
+    },
+];
+
+export const extensions = [
+    {
+        style: "tape-in extensions",
+        min: 300,
+        max: 999,
+    },
+    {
+        style: "clip-in extensions",
+        min: 15,
+        max: 40,
+    },
+    {
+        style: "braids",
+        min: 15,
+        max: 30,
+    },
+];
+
+export const special = [
+    {
+        style: "bridal hair (trial + day of",
+        min: 200,
+        max: 350,
+    },
+    {
+        style: "bridal party hair",
+        min: 75,
+        max: 120,
+    },
+    {
+        style: "makeup application",
+        min: 40,
+        max: 75,
+    },
+];
